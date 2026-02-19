752ffd3768f9e34d668fe6129cbceeb57a44696c
John T. Wodder II <git@varonathe.org>
2026-02-19 09:42:42 -0500

    Packaging boilerplate

---
+++ src/chesthulhu/__init__.py
@@ -0,0 +1,11 @@
+"""
+Extract chest information from a Terraria .wld file
+
+Visit <https://github.com/jwodder/chesthulhu> for more information.
+"""
+
+__version__ = "0.0.0"
+__author__ = "John Thorvald Wodder II"
+__author_email__ = "chesthulhu@varonathe.org"
+__license__ = "MIT"
+__url__ = "https://github.com/jwodder/chesthulhu"
