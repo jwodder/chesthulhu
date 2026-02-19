53ecd70514fbd0185ef16bd8e380895f39ff8c60
John T. Wodder II <git@varonathe.org>
2026-02-19 09:52:55 -0500

    Use versioningit

--- src/chesthulhu/__init__.py
+++ src/chesthulhu/__init__.py
@@ -4,7 +4,9 @@ Extract chest information from a Terraria .wld file
 Visit <https://github.com/jwodder/chesthulhu> for more information.
 """
 
-__version__ = "0.0.0"
+from importlib.metadata import version
+
+__version__ = version("chesthulhu")
 __author__ = "John Thorvald Wodder II"
 __author_email__ = "chesthulhu@varonathe.org"
 __license__ = "MIT"
